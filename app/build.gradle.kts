ed51bb71275eb36a6bdc3732dbc95cdbe29361eb
Laurent Toutain <Laurent@Touta.in>
2025-12-22 15:06:54 +0100

    Rename app to RDMR, update ID, and change Radio 1 logo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.radioapp"
+        applicationId = "com.radioapp.rdmr"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
